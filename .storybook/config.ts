aaada69b8e186cbedafe03549606cc7fb9828d10
Thomas van der velde <contact@toolbarthomas.net>
2019-11-28 11:21:41 +0100

    Initial setup for styled components

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,3 +1,11 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
 
-configure(require.context("../src"), true, /\.stories\.js$/, module);
+configure(require.context('../src', true, /\.stories\.tsx$/), module)
+
+addDecorator(
+  withInfo({
+    inline: true,
+    propTables: false
+  })
+)
